b878dc669000790523832f80bfb4cdf8ce88ada9
Rick Briganti <jeviolle@gmail.com>
2013-08-03 20:19:03 -0400

    updated variable names to make more sense

--- template.R
+++ template.R
@@ -1,4 +1,4 @@
-nba <- read.csv("RESULTS",sep=",")
-row.names(nba) <- nba$Site
-nba_matrix <- data.matrix(nba)
-nba_heatmap <- heatmap(nba_matrix, Rowv=NA, Colv=NA, col=cm.colors(256), scale="column", margins=c(5,10))
+his <- read.csv("RESULTS",sep=",")
+row.names(his) <- his$Site
+his_matrix <- data.matrix(his)
+his_heatmap <- heatmap(his_matrix, Rowv=NA, Colv=NA, col=cm.colors(256), scale="column", margins=c(5,10))
